981838d483517a58605fde530aa33bd93da00e04
LyeAmanda955 <LyeAmanda955@gmail.com>
2016-03-20 20:29:55 -0400

    ok

--- js/getNav.js
+++ js/getNav.js
@@ -1,14 +1,17 @@
-alert(0);
-$.get("http://LyeAmanda955.github.io/Partials/nav.html",
-function(data) {
-$(document).ready(function() {alert(1)
-//check if this document has .container
-var container = $(".container");
-//if so add nav.html contents to the top
-if (container) {
-container.prepend(data)
-//container.fadeIn();
 
-}
-});
-});
+$.get("http://LyeAmanda955.github.io/Partials/nav.html", function(data) {
+
+  $(document).ready(function() {
+
+    //check if this document has .container
+    var container = $(".container");
+
+    //if so add nav.html contents to the top
+    if (container) {
+      container.prepend(data)
+      container.fadeIn();
+    }
+
+  });
+
+});
