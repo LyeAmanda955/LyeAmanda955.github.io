c4c9f0083638dc2a78c3d11e2dabad9e4a0ac9a6
LyeAmanda955 <LyeAmanda955@gmail.com>
2016-03-20 20:05:41 -0400

    added nav

---
+++ js/getNav.js
@@ -0,0 +1,12 @@
+$.get("http://LyeAmanda955.github.io/Partials/nav.html",
+function(data) {
+$(document).ready(function() {
+//check if this document has .container
+var container = $(".container");
+//if so add nav.html contents to the top
+if (container) {
+container.prepend(data)
+//container.fadeIn();
+}
+});
+});
